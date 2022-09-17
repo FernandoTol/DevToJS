d5be25eafb6b0195c59ab813e4960be7f979b4d7
FernandoToledo <toledo.lufe@gmail.com>
2022-09-17 02:30:39 -0500

    add credenciales conection

---
+++ js/credenciales.js
@@ -0,0 +1,18 @@
+// * Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+// TODO: import { getFirestore, addDoc, deleteDoc, updateDoc, getDocs, collection, doc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// *  Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCagJFZ_bUsxlFywdIhDYGqPqKgVHKsbeo",
+    authDomain: "devto-retojs.firebaseapp.com",
+    projectId: "devto-retojs",
+    storageBucket: "devto-retojs.appspot.com",
+    messagingSenderId: "1019303882414",
+    appId: "1:1019303882414:web:2cd31fe2dcd4834582073f"
+};
+
+// * Initialize Firebase
+export const app = initializeApp(firebaseConfig);
